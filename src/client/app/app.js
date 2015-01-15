004c0c34501e4033ed3809ef653c2f280ec0cc71
Kelly Domico <kelly.domico@hp.com>
2015-01-15 15:53:46 -0800

    Fixed cell height issue on expand/collapse

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -14,9 +14,67 @@
 
   var app = angular.module('app', [ 'smart-table', 'lrDragNDrop' ]);
 
+  app.factory('DOMUtilService', function($rootScope) {
+
+    var domUtils = {
+      getElementBox: getElementBox,
+      adjustExpandedRows: adjustExpandedRows
+    };
+
+    return domUtils;
+
+    function getElementBox(element, orientation) {
+
+      var elementPadBorder = { border: 0, padding: 0 };
+
+      var elementComputedStyle = window.getComputedStyle(element);
+
+      var orientKeys = orientation === 'vertical' ? [ 'top', 'bottom' ] : [ 'left', 'right' ];
+      angular.forEach(orientKeys, function(pos, i) {
+        var borderKey = 'border-' + pos + '-width';
+        var borderValue = parseFloat(elementComputedStyle.getPropertyValue(borderKey));
+        elementPadBorder[borderKey] = borderValue;
+        elementPadBorder.border += borderValue;
+
+        var padKey = 'padding-' + pos;
+        var paddingValue = parseFloat(elementComputedStyle.getPropertyValue(padKey));
+        elementPadBorder[padKey] = paddingValue;
+        elementPadBorder.padding += paddingValue;
+      });
+
+      var widthOrHeight = orientation === 'vertical' ? 'height' : 'width';
+      elementPadBorder[widthOrHeight] = parseFloat(elementComputedStyle.getPropertyValue(widthOrHeight));
+
+      return elementPadBorder;
+    }
+
+    function adjustExpandedRows(table) {
+      var expandedRows = table.find('tbody')[0].querySelectorAll('.expanded');
+
+      if (expandedRows.length > 0) {
+        var hasSelection = table.hasClass('select-table');
+
+        var summaryFirstCell = expandedRows[0].previousSibling.previousSibling.querySelector('td:first-child');
+        var summaryRowCell = hasSelection ? summaryFirstCell.nextElementSibling : summaryFirstCell;
+        var summaryRowCellBox = getElementBox(summaryRowCell, 'vertical');
+        var summaryRowCellHeight = summaryRowCellBox.height + summaryRowCellBox.border;
+
+        angular.forEach(expandedRows, function(expandedRow) {
+          var summaryRow = expandedRow.previousSibling.previousSibling;
+          var expandedFirstCell = expandedRow.querySelector('td:first-child');
+          var expandedCell = hasSelection ? expandedFirstCell.nextElementSibling : expandedFirstCell;
+          var expandedCellBox = getElementBox(expandedCell, 'vertical');
+          var actionColHeight = summaryRowCellHeight + expandedCellBox.height + expandedCellBox.border;
+          summaryRow.querySelector('td:last-child').style.height = actionColHeight + 'px';
+        });
+      }
+    }
+
+  });
+
   app.controller('MainCtrl', [ '$scope', MainCtrl ]);
 
-  app.directive('responsiveSmartTable', [ '$window', '$timeout', function($window, $timeout) {
+  app.directive('responsiveSmartTable', [ '$window', '$timeout', 'DOMUtilService', function($window, $timeout, DOMUtilService) {
 
     var directive = {
       restrict: 'A',
@@ -42,18 +100,24 @@
         scope.lastWidth = $window.innerWidth;
 
         if (element.hasClass("action-table")) {
-          var actionColHeader = element.find('thead')[0].querySelector('tr:last-child th:last-child');
-          var actionColHeaderWidth = actionColHeader.offsetWidth - 2;
           var actionCol = element.find('tbody')[0].querySelector('tr:first-child td:last-child');
-          var actionColWidth = actionCol.offsetWidth - 2;
-          var largerWidth = Math.max(actionColHeaderWidth, actionColWidth);
+          var actionColBox = DOMUtilService.getElementBox(actionCol, 'horizontal');
+          var rightBorderWidth = actionColBox['border-right-width'];
+          var actionColWidth = actionCol.scrollWidth;
+
+          var actionColHeader = element.find('thead')[0].querySelector('tr:last-child th:last-child');
+          var actionColHeaderBox = DOMUtilService.getElementBox(actionColHeader, 'horizontal');
+          var actionColHeaderWidth = actionColHeader.scrollWidth;
+
+          var largerWidth = Math.max(actionColHeaderWidth, actionColWidth) - rightBorderWidth;
           element.css('padding-right', largerWidth + 'px');
+
           if (actionColHeaderWidth < actionColWidth) {
-            element.find('thead')[0].querySelectorAll('tr th:last-child')[1].style.width = (actionColWidth - 14) + 'px';
+            actionColHeader.style.width = (actionColBox.width + actionColBox.border) + 'px';
           } else {
             var actionCols = element.find('tbody')[0].querySelectorAll('tr td:last-child');
             angular.forEach(actionCols, function(col) {
-              col.style.width = (largerWidth - 16) + 'px';
+              col.style.width = (actionColHeaderBox.width - rightBorderWidth) + 'px';
             });
           }
         }
@@ -63,13 +127,7 @@
         if (element.hasClass('action-table')) {
           var curWidth = $window.innerWidth;
           if (curWidth !== scope.lastWidth) {
-            var expandedRows = element.find('tbody')[0].querySelectorAll('.expanded');
-            angular.forEach(expandedRows, function(expandedRow) {
-              var summaryRowElt = expandedRow.previousSibling.previousSibling;
-              var actionColHeight = expandedRow.offsetTop + expandedRow.offsetHeight - summaryRowElt.offsetTop - 18;
-              summaryRowElt.querySelector('td:last-child').style.height = actionColHeight + 'px';
-            });
-
+            DOMUtilService.adjustExpandedRows(element);
             scope.lastWidth = curWidth;
           }
         }
@@ -78,7 +136,7 @@
 
   }]);
 
-  app.directive('rstExpandable', function() {
+  app.directive('rstExpandable', [ 'DOMUtilService', function(DOMUtilService) {
 
     var directive = {
       restrict: 'A',
@@ -99,19 +157,17 @@
         detailRow.toggleClass('expanded');
 
         if (table.hasClass('action-table')) {
-          var detailRowElt = detailRow[0];
           var summaryRowElt = summaryRow[0];
 
-          if (detailRow.hasClass('expanded')) {
-            var actionColHeight = detailRowElt.offsetTop + detailRowElt.offsetHeight - summaryRowElt.offsetTop - 18;
-            summaryRowElt.querySelector('td:last-child').style.height = actionColHeight + 'px';
-          } else {
+          if (!detailRow.hasClass('expanded')) {
             summaryRowElt.querySelector('td:last-child').style.height = 'inherit';
           }
+
+          DOMUtilService.adjustExpandedRows(table);
         }
       });
     }
 
-  });
+  }]);
 
 })();
